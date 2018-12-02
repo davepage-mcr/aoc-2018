b5f2d9090e5d4940b3a284314a759180f26c9fd9
Dave Page <dave@davepage.me.uk>
2018-12-02 17:51:15 +0000

    replace while loops with "for x in range()" constructs

--- day02/day2-2.py
+++ day02/day2-2.py
@@ -4,9 +4,8 @@ import sys
 
 def compare( first_id, second_id ):
     print("DEBUG: Comparing", first_id, "with", second_id)
-    pos = 0
     diffs = 0
-    while pos < len(first_id):
+    for pos in range(len(first_id)):
         if ( first_id[pos] != second_id[pos] ):
             if ( diffs > 0 ):
                 print("\tSecond difference between chars, can't be our one", first_id[pos], second_id[pos])
@@ -14,7 +13,6 @@ def compare( first_id, second_id ):
             else:
                 print("\tFirst difference between", first_id[pos], second_id[pos])
                 diffs += 1
-        pos += 1
     if ( diffs == 1 ):
         print("\tExactly one difference between", first_id, second_id)
         return True
@@ -22,24 +20,18 @@ def compare( first_id, second_id ):
     return False
 
 def commonchars( first_id, second_id ):
-    pos = 0
     output = ''
-    while pos < len(first_id):
+    for pos in range(len(first_id)):
       if ( first_id[pos] == second_id[pos] ):
           output += first_id[pos]
-      pos += 1
     return output
 
 # Read the input from the file name specified on the command line
 inputfile = open(sys.argv[1])
 boxids = [ line.strip() for line in inputfile ]
 
-start = 0
-while start < len(boxids):
-    compto = start + 1
-    while compto < len(boxids):
+for start in range(len(boxids)):
+    for compto in range(start, len(boxids)):
         if compare( boxids[start], boxids[compto] ):
             print(commonchars( boxids[start], boxids[compto] ))
             sys.exit()
-        compto += 1
-    start += 1
