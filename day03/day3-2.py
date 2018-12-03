b386498afb895c00ab11a1e3bb761e4f66d9f89d
Dave Page <dave@davepage.me.uk>
2018-12-03 21:25:40 +0000

    Revert back to one loop

--- day03/day3-2.py
+++ day03/day3-2.py
@@ -18,22 +18,19 @@ multiclaims = 0
 
 # Read the input from the file name specified on the command line
 inputfile = open(sys.argv[1])
-inputdata = []
+
 for line in inputfile:
     # #1 @ 1,3: 4x4
     parse = re.match(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', line)
     if not parse:
         print("Line does not match RE:", line.strip())
         next
-    else:
-        inputdata.append( [ int(parse.group(1)),
-                            [ int(x) for x in parse.group(2,3) ],
-                            [ int(x) for x in parse.group(4,5) ] ] )
 
-for datum in inputdata:
-    claim_id =  datum[0]
-    starts =    datum[1]
-    size =      datum[2]
+    claim_id    = int(parse.group(1))
+    starts      = [ int(x) for x in parse.group(2,3) ]
+    size        = [ int(x) for x in parse.group(4,5) ]
+
+    nooverlap   = True
     for x in range(starts[0], starts[0]+size[0]):
         for y in range(starts[1], starts[1]+size[1]):
             if len(fabric[x][y]) == 0:
@@ -46,19 +43,7 @@ print(multiclaims, "square inches contested by multiple claims")
 
 # Now go back over input data and check where there's only one claim_id in all the squares
 
-nooverlaps = []
-for datum in inputdata:
-    claim_id =  datum[0]
-    starts =    datum[1]
-    size =      datum[2]
-    overlaps = False
-    for x in range(starts[0], starts[0]+size[0]):
-        for y in range(starts[1], starts[1]+size[1]):
-            if len(fabric[x][y]) != 1:
-                overlaps = True
-                # We should be able to jump out of this double-nested loop
-    
-    if not overlaps:
-        nooverlaps.append(claim_id)
+    if nooverlap:
+        nooverlaps.add( claim_id )
 
 print("No overlaps in claims", nooverlaps)
